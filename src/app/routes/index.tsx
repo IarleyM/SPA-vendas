493a350601159833ee15f26e8c1b1ffb9fa2f5da
IarleyM <iarleym771@gmail.com>
2025-07-24 16:27:46 -0300

    Ajustando rotas em index.tsx

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes as Switch, Navigate } from "react-router-dom";
-import { Dashboard, ListaVendas, Login, PainelVendedor } from "../pages";
+import { PainelVendedor } from "../pages";
 import { PainelVendas } from "../pages/painel-vendas/PainelVendas";
 import { DetalhesVendas } from "../pages/detalhe-venda/DetalhesVendas";
 import { PainelProdutos } from "../pages/painel-produtos/PainelProdutos";
@@ -8,15 +8,12 @@ import { AdicionarVendas } from "../pages/adicionar-vendas/AdicionarVendas";
 export const Routes = () => {
   return (
     <Switch>
-      <Route path="/tela-inicial" element={<Dashboard />} />
-      <Route path="/grid-vendas" element={<Login />} />
-      <Route path="/lista-vendas" element={<ListaVendas />} />
       <Route path="/painel-vendas" element={<PainelVendas />} />
       <Route path="/painel-vendedor" element={<PainelVendedor />} />
       <Route path="/detalhes-vendas/:id" element={<DetalhesVendas />} />
       <Route path="/painel-produtos" element={<PainelProdutos />} />
       <Route path="/adicionar-vendas" element={<AdicionarVendas />} />
-      <Route path="*" element={<Navigate to="/grid-vendas" replace />} />
+      <Route path="*" element={<Navigate to="/painel-vendas" replace />} />
     </Switch>
   );
 }
